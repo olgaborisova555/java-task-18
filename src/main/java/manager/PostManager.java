57d07e6a890f902ffca8d08d9e90a14ecec95f46
olgaborisova555 <olga.borisova803@gmail.com>
2021-01-05 19:24:23 +0300

    added package manager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,16 @@
+package manager;
+import vk.*;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset){
+        return null;
+    }
+
+    public void delete(int ownerId, int postId){
+
+    }
+}
+
+
